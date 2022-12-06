9417405d8f3b565afa9972b4185be83545e18663
klekovvlad <100075182+klekovvlad@users.noreply.github.com>
2022-12-06 12:39:07 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -107,3 +107,123 @@ const swiper = new Swiper('.swiper', {
         }
     }
 });
+
+window.addEventListener('scroll', () => {
+  let windowCenter = window.innerHeight * 0.75;
+  const animateUp = document.querySelectorAll('.animate-up');
+  const amimateDown = document.querySelectorAll('.animate-down');
+  animateUp.forEach(el => {
+    let position = el.getBoundingClientRect().top - windowCenter;
+    if (position <= 0) {
+      el.classList.add('animate');
+    };
+  });
+  amimateDown.forEach(el => {
+    let position = el.getBoundingClientRect().top - windowCenter;
+    if (position <= 0) {
+      el.classList.add('animate');
+    };
+  })
+});
+
+function sideAnimation() {
+  const animateLeft = document.querySelectorAll('.animate-left');
+  animateLeft.forEach(el => {
+    el.classList.add('animate');
+  })
+};
+
+sideAnimation();
+
+const mainMoney = document.querySelector('.main-form-money');
+const mainTariff = document.querySelector('.main-form-tariff');
+const errorMessage = document.querySelector('.error-message');
+mainMoney.addEventListener('keyup', () => {
+  mainMoney.value = mainMoney.value.replace(/[^\d]/g,'');
+  
+  if(mainMoney.value <= 30000) {
+    mainTariff.innerHTML = 'Бесплатный';
+  }
+  if(30001 <= mainMoney.value && mainMoney.value <= 60000) {
+    mainTariff.innerHTML = 'Продвинутый';
+  }
+  if(mainMoney.value > 60000) {
+    mainTariff.innerHTML = 'Профессиональный';
+  }
+  if(mainMoney.value > 100001) {
+    mainMoney.value = '';
+    mainTariff.innerHTML = '';
+    mainMoney.classList.add('input-error');
+    errorMessage.innerHTML = 'до 100 000 руб.'
+  }
+});
+mainMoney.addEventListener('click', () => {
+  mainMoney.classList.remove('input-error');
+  errorMessage.innerHTML = ''
+});
+mainMoney.addEventListener('blur', () => {
+  if(mainMoney.value < 5000) {
+    mainMoney.value = '';
+    mainTariff.innerHTML = '';
+    mainMoney.classList.add('input-error');
+    errorMessage.innerHTML = 'от 5 000 руб.'
+  }
+})
+
+const hour = {
+  hour: 'numeric'
+};
+const min = {
+  minute: 'numeric'
+};
+const currentTime = document.querySelector('.speed__item-current');
+const resolTime = document.querySelector('.speed__item-agree');
+const moneyTime = document.querySelector('.speed__item-money');
+
+function createTimes() {
+  currentHour = document.createElement('span');
+  currentMin = document.createElement('span');
+  currentTime.append(currentHour, ':', currentMin);
+  resolHour = document.createElement('span');
+  resolMin = document.createElement('span');
+  resolTime.append(resolHour, ':', resolMin);
+  moneyHour = document.createElement('span');
+  moneyMin = document.createElement('span');
+  moneyTime.append(moneyHour, ':', moneyMin);
+};
+function updateTimes() {
+  let t = new Date();
+  let minute = Number(t.toLocaleString('ru',min));
+  currentHour.innerHTML = Number(t.toLocaleString('ru',hour));
+  currentMin.innerHTML = minute;
+  resolHour.innerHTML = Number(t.toLocaleString('ru',hour));
+  resolMin.innerHTML = minute + 5;
+  moneyHour.innerHTML = Number(t.toLocaleString('ru',hour));
+  moneyMin.innerHTML = minute + 10;
+  if ((minute + 5) >= 60) {
+    resolHour.innerHTML = Number(t.toLocaleString('ru',hour)) + 1;
+    resolMin.innerHTML = '05';
+  };
+  if ((minute + 10) >= 60) {
+    moneyHour.innerHTML = Number(t.toLocaleString('ru',hour)) + 1;
+    moneyMin.innerHTML = '08';
+  };
+  if (resolHour.innerHTML === '24') {
+    resolHour.innerHTML = '00'
+  };
+  if (moneyHour.innerHTML === '24') {
+    moneyHour.innerHTML = '00'
+  };
+  if (minute < 10) {
+    currentMin.innerHTML = '0' + minute;
+  };
+  if ((minute + 5) < 10) {
+    resolMin.innerHTML = '0' + (minute + 5);
+  };
+  if ((minute + 10) < 10) {
+    moneyMin.innerHTML = '0' + (minute + 8);
+  };
+};
+createTimes();
+updateTimes();
+setInterval(updateTimes, 1000);
